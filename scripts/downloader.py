e7c256e80d5e0f0096ce1a5f4be35b2b318976ac
ry10hu <115399801+ry10hu@users.noreply.github.com>
2023-12-14 11:51:09 -0800

    Update downloader.py

--- scripts/downloader.py
+++ scripts/downloader.py
@@ -10,9 +10,9 @@ os.system('sudo chmod a+rx /usr/local/bin/youtube-dl')
 #Defining A Function (Makes Typing This All Out Easier)
 def fetch(videolist, songlist):
     for videourl in videolist:
-        os.system(f'youtubedl {videourl}')
+        os.system(f'youtube-dl {videourl}')
     for songurl in songlist:
-        os.system(f'youtubedl --extract-audio {songurl}')
+        os.system(f'youtube-dl --extract-audio {songurl}')
 ##Download List
 ###When Adding Urls, Make Sure To Make It A String With '' Or "" Ex: "https://www.youtube.com/watch?v=3Z5aqsvNsLQ"
 videolist = ['https://www.youtube.com/watch?v=3Z5aqsvNsLQ'
