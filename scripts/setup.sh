3e8f5af704a6f1b0790681daa13f08baddf9df8f
ry10hu <115399801+ry10hu@users.noreply.github.com>
2023-12-15 09:56:49 -0800

    Update and rename downloader.sh to setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,2 +1,4 @@
+## This Script Will Install Dependencies Required For My Scripts And Such
+
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
